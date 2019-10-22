26cda58210e84e7a02820f10028beb77229e9d4a
Auge <auge.zaveckaite@vinted.com>
2019-10-22 14:41:39 +0300

    change p to span

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
+        <span>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
+        </span>
         <a
           className="App-link"
           href="https://reactjs.org"
