a6f723abd0fa480990ba20cf4cb50ac8ae100e02
Auge <auge.zaveckaite@vinted.com>
2019-10-22 14:50:41 +0300

    change functional component to class component fix errors

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import "./App.scss";
 
-class App extends Component<>  {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <span>
-          Edit <code>src/App.js</code> and save to reload.
-        </span>
-        <p
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React and more
-        </p>
-      </header>
-    </div>
-  );
+class App extends Component<> {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <span>
+            Edit <code>src/App.js</code> and save to reload.
+          </span>
+          <p
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React and more
+          </p>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
